5c263635e079637dd7fc16e7290a447432bfd0db
chichunchen <chichunchen844@gmail.com>
2015-12-04 02:07:57 +0800

    Scaffold hw3.cgi

---
+++ cgi/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dbg.h"
+
+int main(int argc, const char *argv[])
+{
+    printf("Content-Type: text/plain\n\n");
+    printf("Hello!\r\n");
+
+    return 0;
+}
