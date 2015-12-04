79f159afff27b348c3f1279b1546fb42acb0fa99
chichunchen <chichunchen844@gmail.com>
2015-12-04 16:15:09 +0800

    Refactor cgi code.

--- httpd/src/cgi.c
+++ httpd/src/cgi.c
@@ -51,80 +51,34 @@ int readfile_into_buf(char *filename, char *buf)
     return 0;
 }
 
-int cgi_handler(int connfd, Request *request)
+/**
+ * execute cgi program
+ */
+
+void cgi_exec(int connfd, char *path, Request *request)
 {
     pid_t pid;
     pid = Fork();
 
     if (pid == 0) {                 /* child */
 
-        char buf[BUF_SIZE], params[BUF_SIZE];
-        Read(connfd, buf, BUF_SIZE);
-        sprintf(request->content_length, "%ld", strlen(buf));
-        fprintf(stderr, "%s\n", buf);
-        fprintf(stderr, "\n==================== PARSE RESULT ======================\n");
-        fprintf(stderr, "Content-Length=%s\n", request->content_length);
-        parse_request_method(buf, request);
-        fprintf(stderr, "request_method=%s\n", request->request_method);
-        parse_params(params, request);
-        fprintf(stderr, "params=%s\n", params);
-        fprintf(stderr, "============================= END ========================\n");
+        write_cgi_header(connfd);
+        Dup2(connfd, STDIN_FILENO);
+        Dup2(connfd, STDOUT_FILENO);
 
-        char root[50] = "../www/";
+        //strcat(root, params);
+        fprintf(stderr, "path=%s", path);
 
-        switch (parse_extension(params)) {
-            case CGI:
-                write_cgi_header(connfd);
-                Dup2(connfd, STDIN_FILENO);
-                Dup2(connfd, STDOUT_FILENO);
+        /* set cgi env */
+        //setenv2("QUERY_STRING", request->query_string);
+        setenv2("CONTENT_LENGTH", request->content_length);
+        setenv2("REQUEST_METHOD", request->request_method);
+        //setenv2("SCRIPT_NAME", request->script_name);
+        //setenv2("REMOTE_HOST", request->remote_host);
+        setenv2("REMOTE_ADDR", request->remote_addr);
 
-                strcat(root, params);
-                fprintf(stderr, "root=%s", root);
-
-                /* set cgi env */
-                //setenv2("QUERY_STRING", request->query_string);
-                setenv2("CONTENT_LENGTH", request->content_length);
-                setenv2("REQUEST_METHOD", request->request_method);
-                //setenv2("SCRIPT_NAME", request->script_name);
-                //setenv2("REMOTE_HOST", request->remote_host);
-                setenv2("REMOTE_ADDR", request->remote_addr);
-
-                if (execl(root, "", NULL) < 0) {
-                    write_bad_request(connfd);
-                }
-
-                break;
-
-            case HTML:
-                strcat(root, params);
-                fprintf(stderr, "html root=%s", root);
-
-                /* read file into buf */
-                if (readfile_into_buf(root, buf) == -1) {
-                    log_warn("html not found");
-                    write_bad_request(connfd);
-                    break;
-                }
-
-                write_html_header(connfd);
-                Write(connfd, buf, strlen(buf));
-
-                break;
-
-            default:
-                /* request with other extentions */
-                strcat(root, params);
-                fprintf(stderr, "html root=%s", root);
-
-                /* read file into buf */
-                if (readfile_into_buf(root, buf) == -1) {
-                    log_warn("not found");
-                    write_bad_request(connfd);
-                    break;
-                }
-
-                write_html_header(connfd);
-                Write(connfd, buf, strlen(buf));
+        if (execl(path, "", NULL) < 0) {
+            write_bad_request(connfd);
         }
 
         exit(EXIT_SUCCESS);
@@ -133,7 +87,63 @@ int cgi_handler(int connfd, Request *request)
 
         int return_val;
         waitpid(pid, &return_val, 0);
-        if (WEXITSTATUS(return_val) == 1) return 1;
+        if (WEXITSTATUS(return_val) == 1) return;
+    }
+}
+
+int cgi_handler(int connfd, Request *request)
+{
+    char buf[BUF_SIZE], params[BUF_SIZE];
+    Read(connfd, buf, BUF_SIZE);
+    sprintf(request->content_length, "%ld", strlen(buf));
+    fprintf(stderr, "%s\n", buf);
+    fprintf(stderr, "\n==================== PARSE RESULT ======================\n");
+    fprintf(stderr, "Content-Length=%s\n", request->content_length);
+    parse_request_method(buf, request);
+    fprintf(stderr, "request_method=%s\n", request->request_method);
+    parse_params(params, request);
+    fprintf(stderr, "params=%s\n", params);
+    fprintf(stderr, "============================= END ========================\n");
+
+    char root[50] = "../www/";
+
+    switch (parse_extension(params)) {
+        case CGI:
+            strcat(root, params);
+            cgi_exec(connfd, root, request);
+
+            break;
+
+        case HTML:
+            strcat(root, params);
+            fprintf(stderr, "html root=%s", root);
+
+            /* read file into buf */
+            if (readfile_into_buf(root, buf) == -1) {
+                log_warn("html not found");
+                write_bad_request(connfd);
+                break;
+            }
+
+            write_html_header(connfd);
+            Write(connfd, buf, strlen(buf));
+
+            break;
+
+        default:
+            /* request with other extentions */
+            strcat(root, params);
+            fprintf(stderr, "html root=%s", root);
+
+            /* read file into buf */
+            if (readfile_into_buf(root, buf) == -1) {
+                log_warn("not found");
+                write_bad_request(connfd);
+                break;
+            }
+
+            write_html_header(connfd);
+            Write(connfd, buf, strlen(buf));
     }
 
     Close(connfd);
--- httpd/src/request.c
+++ httpd/src/request.c
@@ -1,2 +1,1 @@
 #include "unp.h"
-
