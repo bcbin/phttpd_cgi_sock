5581f7887fe4399853c4c2dbb64da9fe60cce3cc
chichunchen <chichunchen844@gmail.com>
2016-01-16 00:00:26 +0800

    create a new thread while new connection come in.

--- httpd/src/cgi.c
+++ httpd/src/cgi.c
@@ -83,77 +83,77 @@ void cgi_exec(int connfd, char *path, Request *request)
 int cgi_handler(int connfd, Request *request)
 {
     char buf[BUF_SIZE], params[BUF_SIZE];
-    Read(connfd, buf, BUF_SIZE);
+    //Read(connfd, buf, BUF_SIZE);
 
     /* set content_length */
     char *content_length = Malloc(sizeof(ssize_t));
-    sprintf(content_length, "%ld", strlen(buf));
-    setenv2("CONTENT_LENGTH", content_length);
-
-    /* parse request_method and remote_addr */
-    parse_request_method(buf, request);
-    setenv2("REMOTE_ADDR", request->remote_addr);
-
-    /* set arbitrary env for cgi program */
-    setenv2("SCRIPT_NAME", "/~0453411/public_html/");
-    setenv2("REMOTE_HOST", "nplinux2.cs.nctu.edu.tw");
-    setenv2("AUTH_TYPE", "normal");
-    setenv2("REMOTE_USER", "guest");
-    setenv2("REMOTE_IDENT", "unknown");
-
-    /* parse program path */
-    parse_params(params, request);
-    char root[50] = "../www/";
-
-    if (DEBUG) {
-        //fprintf(stderr, "%s\n", buf);
-        fprintf(stderr, "\n==================== PARSE RESULT ======================\n");
-        fprintf(stderr, "Content-Length=%s\n", content_length);
-        // fprintf(stderr, "request_method=%s\n", request->request_method);
-        fprintf(stderr, "params=%s\n", params);
-        fprintf(stderr, "============================= END ========================\n");
-    }
-
-
-    switch (parse_extension(params)) {
-        case CGI:
-            strcat(root, params);
-            cgi_exec(connfd, root, request);
-
-            break;
-
-        case HTML:
-            strcat(root, params);
-            fprintf(stderr, "html root=%s", root);
-
-            /* read file into buf */
-            if (readfile_into_buf(root, buf) == -1) {
-                log_warn("html not found");
-                write_bad_request(connfd);
-                break;
-            }
-
-            write_html_header(connfd);
-            Write(connfd, buf, strlen(buf));
-
-            break;
-
-        default:
-            /* request with other extentions */
-            strcat(root, params);
-            fprintf(stderr, "html root=%s", root);
-
-            /* read file into buf */
-            if (readfile_into_buf(root, buf) == -1) {
-                log_warn("not found");
-                write_bad_request(connfd);
-                break;
-            }
-
-            write_html_header(connfd);
-            Write(connfd, buf, strlen(buf));
-    }
-
-    Close(connfd);
+//     sprintf(content_length, "%ld", strlen(buf));
+//     setenv2("CONTENT_LENGTH", content_length);
+// 
+//     /* parse request_method and remote_addr */
+//     parse_request_method(buf, request);
+//     setenv2("REMOTE_ADDR", request->remote_addr);
+// 
+//     /* set arbitrary env for cgi program */
+//     setenv2("SCRIPT_NAME", "/~0453411/public_html/");
+//     setenv2("REMOTE_HOST", "nplinux2.cs.nctu.edu.tw");
+//     setenv2("AUTH_TYPE", "normal");
+//     setenv2("REMOTE_USER", "guest");
+//     setenv2("REMOTE_IDENT", "unknown");
+// 
+//     /* parse program path */
+//     parse_params(params, request);
+//     char root[50] = "../www/";
+// 
+//     if (DEBUG) {
+//         //fprintf(stderr, "%s\n", buf);
+//         fprintf(stderr, "\n==================== PARSE RESULT ======================\n");
+//         fprintf(stderr, "Content-Length=%s\n", content_length);
+//         // fprintf(stderr, "request_method=%s\n", request->request_method);
+//         fprintf(stderr, "params=%s\n", params);
+//         fprintf(stderr, "============================= END ========================\n");
+//     }
+// 
+// 
+//     switch (parse_extension(params)) {
+//         case CGI:
+//             strcat(root, params);
+//             cgi_exec(connfd, root, request);
+// 
+//             break;
+// 
+//         case HTML:
+//             strcat(root, params);
+//             fprintf(stderr, "html root=%s", root);
+// 
+//             /* read file into buf */
+//             if (readfile_into_buf(root, buf) == -1) {
+//                 log_warn("html not found");
+//                 write_bad_request(connfd);
+//                 break;
+//             }
+// 
+//             write_html_header(connfd);
+//             Write(connfd, buf, strlen(buf));
+// 
+//             break;
+// 
+//         default:
+//             /* request with other extentions */
+//             strcat(root, params);
+//             fprintf(stderr, "html root=%s", root);
+// 
+//             /* read file into buf */
+//             if (readfile_into_buf(root, buf) == -1) {
+//                 log_warn("not found");
+//                 write_bad_request(connfd);
+//                 break;
+//             }
+// 
+//             write_html_header(connfd);
+//             Write(connfd, buf, strlen(buf));
+//     }
+// 
+    //Close(connfd);
     return 0;
 }
--- httpd/src/httpd.c
+++ httpd/src/httpd.c
@@ -1,41 +1,71 @@
 #include "unp.h"
 
+typedef struct _conn {
+    int connfd;
+    struct sockaddr_in cliaddr;
+} Connection;
+
+static void *
+doit(void *arg)
+{
+    int connfd;
+    struct sockaddr_in cliaddr;
+    Connection conn;
+
+    conn = *((Connection *) arg);
+    connfd = conn.connfd;
+    cliaddr = conn.cliaddr;
+    log_info("connfd=%d, cliaddr=%d", connfd, cliaddr.sin_port);
+
+    free(arg);
+
+    Request *request = Malloc(sizeof(request));
+    strcpy(request->remote_addr, inet_ntoa(cliaddr.sin_addr));
+    cgi_handler(connfd, request);
+
+    pthread_detach(pthread_self());
+    Close(connfd);
+    return (NULL);
+}
+
 int main(int argc, const char *argv[])
 {
-    int     listenfd, connfd;
+    int     listenfd;
+    Connection  *conn;
+    pthread_t tid;
     socklen_t clilen;
-    struct sockaddr_in cliaddr;
+    struct sockaddr_in cliaddr, servaddr;
 
     /* all init work */
     signal_init();
 
 	listenfd = tcp_listen();
 
-    /* Listen */
-    Listen(listenfd, LISTENQ);
-
     log_info("Start listen on port %d", SERV_PORT);
 
-    for( ; ; ) {
+    for(;;) {
+        conn = Malloc(sizeof(Connection));
         clilen = sizeof(cliaddr);
-        connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
-
-        /* Fork to handle new connections */
-        pid_t pid;
-        pid = Fork();
-
-        if (pid == 0) {             /* child */
-
-            Request *request = Malloc(sizeof(request));
-            strcpy(request->remote_addr, inet_ntoa(cliaddr.sin_addr));
-            cgi_handler(connfd, request);
-
-            exit(EXIT_SUCCESS);
-
-        } else {                    /* parend */
-
-            Close(connfd);
-        }
+        conn->connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
+        conn->cliaddr = cliaddr;
+        pthread_create(&tid, NULL, &doit, conn);
+// 
+//         /* Fork to handle new connections */
+//         pid_t pid;
+//         pid = Fork();
+// 
+//         if (pid == 0) {             /* child */
+// 
+//             Request *request = Malloc(sizeof(request));
+//             strcpy(request->remote_addr, inet_ntoa(cliaddr.sin_addr));
+//             cgi_handler(connfd, request);
+// 
+//             exit(EXIT_SUCCESS);
+// 
+//         } else {                    /* parend */
+// 
+//             Close(connfd);
+//         }
     }
 
     /* should never go here */
